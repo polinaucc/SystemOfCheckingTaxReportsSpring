0a94e56740e5da73ff2cb1d8e42d0200203089bd
Polina <polinaucc@gmail.com>
2020-02-07 03:55:28 +0200

    added spring security web app config

--- src/main/java/ua/polina/finalProject/SystemOfCheckingTaxReports/config/SpringSecurityWebAppConfig.java
+++ src/main/java/ua/polina/finalProject/SystemOfCheckingTaxReports/config/SpringSecurityWebAppConfig.java
@@ -1,92 +1,58 @@
 package ua.polina.finalProject.SystemOfCheckingTaxReports.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import ua.polina.finalProject.SystemOfCheckingTaxReports.security.CustomUserDetailsService;
-import ua.polina.finalProject.SystemOfCheckingTaxReports.security.JwtAuthenticationEntryPoint;
-import ua.polina.finalProject.SystemOfCheckingTaxReports.security.JwtAuthenticationFilter;
 
+@Primary
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    CustomUserDetailsService customUserDetailsService;
-
-    @Autowired
-    private JwtAuthenticationEntryPoint unauthorizedHandler;
-
-    @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter();
+    public SpringSecurityWebAppConfig() {
+        super();
     }
 
     @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder
-                .userDetailsService(customUserDetailsService)
-                .passwordEncoder(passwordEncoder());
-    }
-
-    @Bean(BeanIds.AUTHENTICATION_MANAGER)
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors()
-                .and()
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
                 .csrf()
-                .disable()
-                .exceptionHandling()
-                .authenticationEntryPoint(unauthorizedHandler)
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers("/",
-                        "/favicon.ico",
-                        "/**/*.png",
-                        "/**/*.gif",
-                        "/**/*.svg",
-                        "/**/*.jpg",
-                        "/**/*.html",
-                        "/**/*.css",
-                        "/**/*.js")
-                .permitAll()
-                .antMatchers("/api/auth/**")
-                .permitAll()
-                .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability")
-                .permitAll()
-                .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated();
+                .disable();
 
-        // Add our custom JWT security filter
-        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/", "auth/login", "/logout")
+                .permitAll();
+
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/inspector/**")
+                .access("hasAuthority(T(ua.polina.finalProject.SystemOfCheckingTaxReports.entity.RoleType).INSPECTOR.getAuthority())");
+
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/client/**")
+                .access("hasAuthority(T(ua.polina.finalProject.SystemOfCheckingTaxReports.entity.RoleType).CLIENT.getAuthority())");
+
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/admin/**")
+                .access("hasAuthority(T(ua.polina.finalProject.SystemOfCheckingTaxReports.entity.RoleType).ADMIN.getAuthority())");
+
+        httpSecurity
+                .authorizeRequests()
+                .and()
+                .exceptionHandling();
+
+        httpSecurity
+                .authorizeRequests()
+                .and()
+                .formLogin()
+                .loginPage("/auth/login")
+                .failureUrl("/auth/login?error")
+                .defaultSuccessUrl("/auth/default-success", true)
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/auth/login?logout");
     }
 }
